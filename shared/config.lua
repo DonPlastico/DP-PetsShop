30d5008ad98553660bcc006fa7d9bb1dd28fe8ca
Alkasana23 <alexneitor5@gmail.com>
2025-08-12 20:41:04 +0200

    Solucion de problemas

--- shared/config.lua
+++ shared/config.lua
@@ -25,98 +25,98 @@ Config.items = {
         pet_code = "a_c_cat_01",
         price = "15000",
         cattegory = "others",
-        img = "cat1.png"
+        img = "cat.png"
     },
     [2] = {
         name = "Armadillo",
         pet_code = "armadillo",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "armadillo.png"
     },
     [3] = {
         name = "Jabalí",
         pet_code = "a_c_boar",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "boar.png"
     },
     [4] = {
         name = "Vaca",
         pet_code = "a_c_cow",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "cow.png"
     },
     [5] = {
         name = "Coyote",
         pet_code = "a_c_coyote",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "coyote.png"
     },
     [6] = {
         name = "Ciervo",
         pet_code = "a_c_deer",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "deer.png"
     },
     [7] = {
         name = "Gallina",
         pet_code = "a_c_hen",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "chicken.png"
     },
     [8] = {
         name = "Pantera",
         pet_code = "a_c_mtlion",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "montainlion.png"
     },
     [9] = {
         name = "Cerdo",
         pet_code = "a_c_pig",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "pig.png"
     },
     [10] = {
         name = "Conejo",
         pet_code = "a_c_rabbit_01",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "rabbit.png"
     },
     [11] = {
         name = "Rata",
         pet_code = "a_c_rat",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "rat.png"
     },
     [12] = {
         name = "Oso",
         pet_code = "bear",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "bear.png"
     },
     [13] = {
         name = "Caballo",
         pet_code = "horse",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "horse.png"
     },
     [14] = {
         name = "Cebra",
         pet_code = "zebra",
         price = "20000",
         cattegory = "others",
-        img = ""
+        img = "zebra.png"
     },
 
 
@@ -127,55 +127,55 @@ Config.items = {
         pet_code = "a_c_husky",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog2.png"
     },
     [16] = {
         name = "Poodle",
         pet_code = "a_c_poodle",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog3.png"
     },
     [17] = {
         name = "Pug",
         pet_code = "a_c_pug",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog4.png"
     },
     [18] = {
         name = "Retriever",
         pet_code = "a_c_retriever",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog5.png"
     },
     [19] = {
         name = "Rottweiler",
         pet_code = "a_c_rottweiler",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog6.png"
     },
     [20] = {
         name = "Shepherd",
         pet_code = "a_c_shepherd",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog7.png"
     },
     [21] = {
         name = "Westy",
         pet_code = "a_c_westy",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog8.png"
     },
     [22] = {
         name = "Bulldog",
         pet_code = "bulldog",
         price = "20000",
         cattegory = "dog",
-        img = ""
+        img = "dog9.png"
     },
 }
